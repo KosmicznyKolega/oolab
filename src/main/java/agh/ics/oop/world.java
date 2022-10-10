ed6af00e821145b0696f44f17601a1f934e853c1
Kosmicznykolega <obuchtom@interia.pl>
2022-10-11 01:19:20 +0200

    przerwa 10/24

---
+++ src/main/java/agh/ics/oop/world.java
@@ -0,0 +1,14 @@
+package agh.ics.oop;
+
+public class world {
+
+    public static void run(String[] stuff){
+        System.out.println("zwierzak idzie");
+    }
+        public static void main(String[] args){
+        String[] strarray = new String[]{"zz", "xx"};
+        System.out.println("start");
+        run(strarray);
+        System.out.println("end");
+    }
+}
