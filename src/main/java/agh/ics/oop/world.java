4f9a937678be71fd328e902e7adfc221e4d6818a
Kosmicznykolega <obuchtom@interia.pl>
2022-10-11 11:31:52 +0200

    18/24

--- src/main/java/agh/ics/oop/world.java
+++ src/main/java/agh/ics/oop/world.java
@@ -2,13 +2,30 @@ package agh.ics.oop;
 
 public class world {
 
-    public static void run(String[] stuff){
+    public static void run(Direction[] stuff){
         System.out.println("zwierzak idzie");
+        for (Direction argument:stuff){
+            switch(argument){
+                case FORWARD:
+                    System.out.println("zwierzak idzie do przodu");
+                    break;
+                case BACKWARDS:
+                    System.out.println("zwierzak idzie do tylu");
+                    break;
+                case LEFT:
+                    System.out.println("zwierzak idzie w lewo");
+                    break;
+                case RIGHT:
+                    System.out.println("zwierzak idze w prawo");
+                    break;
+
+                }
+        }
     }
         public static void main(String[] args){
-        String[] strarray = new String[]{"zz", "xx"};
+        Direction[] directions = new Direction[]{Direction.FORWARD, Direction.BACKWARDS};
         System.out.println("start");
-        run(strarray);
+        run(directions);
         System.out.println("end");
     }
 }
