3cf2d2881ff38d561c5e4c05ba0bff3323db0114
KosmicznyKolega <115363268+KosmicznyKolega@users.noreply.github.com>
2022-11-17 15:37:16 +0100

    lab05 poprawiony blad z placeowaniem zwierzat na zwierzetach

--- src/main/java/agh/ics/oop/SimulationEngine.java
+++ src/main/java/agh/ics/oop/SimulationEngine.java
@@ -2,13 +2,13 @@ package agh.ics.oop;
 
 import java.util.ArrayList;
 
-public class SimulationEngine implements IEngine{
+public class SimulationEngine implements IEngine {
     private Vector2d[] positions;
-    private ArrayList<Animal> animals=new ArrayList<Animal>();
+    private ArrayList<Animal> animals = new ArrayList<Animal>();
     private MoveDirection[] movements;
     private IWorldMap map;
 
-    public SimulationEngine( MoveDirection[] movements, IWorldMap map, Vector2d[] positions) {
+    public SimulationEngine(MoveDirection[] movements, IWorldMap map, Vector2d[] positions) {
         this.positions = positions;
         this.movements = movements;
         this.map = map;
@@ -16,14 +16,16 @@ public class SimulationEngine implements IEngine{
 
     @Override
     public void run() {
-        for (Vector2d position:positions){
-            Animal animal=new Animal(map, position);
-            map.place(animal);
-            animals.add(animal);
+        for (Vector2d position : positions) {
+            Animal animal = new Animal(map, position);
+            if (map.place(animal)) {
+                animals.add(animal);
+            }
         }
-        int length = positions.length;
-        for (int i=0;i<movements.length;i++){
-            animals.get(i%length).move(movements[i]);
+        int length = animals.size();
+        for (int i = 0; i < movements.length; i++) {
+            animals.get(i % length).move(movements[i]);
         }
     }
 }
+
